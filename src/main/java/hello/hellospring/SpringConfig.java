d296c00b3aa5911ca76f4b0435a7f94bd97cab3d
juniorsuk <juniorsuk@naver.com>
2024-05-05 18:56:21 +0900

    202405051856

---
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,23 @@
+package hello.hellospring;
+
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository(){
+        // 데이터베이스 선정 안되어있는 상황이라 메모리에 임시로 저장
+        return new MemoryMemberRepository();
+    }
+
+}
